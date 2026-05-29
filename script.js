27ba705a2a9af06dbc4421d8f2c83e89b017ed5c
Maik <maikmuc80@gmail.com>
2026-05-29 07:37:58 +0200

    feat: array add

---
+++ script.js
@@ -0,0 +1,91 @@
+const menu = [
+  // Burger & Sandwiches
+  {
+    category: "Burger & Sandwiches",
+    name: "Veggie mushroom black burger",
+    description: "Mixed green salad, Tomatoes, Edamame, Mushrooms",
+    price: 16.90,
+    amount: 0
+  },
+  {
+    category: "Burger & Sandwiches",
+    name: "All meat burger",
+    description: "Beef, Bacon, Dill pickles, Smoked cheese, Ketchup, BBQ sauce",
+    price: 15.90,
+    amount: 0
+  },
+  {
+    category: "Burger & Sandwiches",
+    name: "Beef red burger",
+    description: "Beef, Cheese, Tomatoes, Lettuce, Onion",
+    price: 14.90,
+    amount: 0
+  },
+  {
+    category: "Burger & Sandwiches",
+    name: "Big chicken burger",
+    description: "Chicken, Cheese, Tomatoes, Lettuce, Onion, Bell pepper",
+    price: 15.90,
+    amount: 0
+  },
+
+  // Pizza (30cm)
+  {
+    category: "Pizza",
+    name: "Pizza Margherita",
+    description: "Tomato Sauce, Mozzarella",
+    price: 11.90,
+    amount: 0
+  },
+  {
+    category: "Pizza",
+    name: "Pizza Chorizo",
+    description: "Tomato slices, Mozzarella, Chorizo",
+    price: 13.90,
+    amount: 0
+  },
+  {
+    category: "Pizza",
+    name: "Funghi",
+    description: "Red onion, Olives, Button Mushrooms, Mozzarella",
+    price: 12.90,
+    amount: 0
+  },
+  {
+    category: "Pizza",
+    name: "Quattro Formaggi with Chicken",
+    description: "Chicken, Mozzarella, Gorgonzola, Fontina, Parmigiano Reggiano",
+    price: 15.90,
+    amount: 0
+  },
+
+  // Salad
+  {
+    category: "Salad",
+    name: "Warm beef arugula salad",
+    description: "Beef, Arugula, Field salad, Greek feta, Cherry tomatoes, Sun-dried Tomatoes, Balsamic-vinegar dressing",
+    price: 16.90,
+    amount: 0
+  },
+  {
+    category: "Salad",
+    name: "Mini green Salad",
+    description: "Green salad, Cucumber, Carrots, Parsley, Radishes",
+    price: 7.90,
+    amount: 0
+  },
+  {
+    category: "Salad",
+    name: "Green Salad with sea food",
+    description: "Mixed greens, Cherry tomatoes, Red onion, Mussels, Squid rings, Shrimp, Dijon mustard-lemon dressing with dill",
+    price: 16.90,
+    amount: 0
+  },
+  {
+    category: "Salad",
+    name: "Vegan green salad with tofu",
+    description: "Green salad, Cherry tomatoes, Cucumber, Baby spinach, Edamame, Radishes, Bittercress, Tofu, Peanuts",
+    price: 14.90,
+    amount: 0
+  }
+];
