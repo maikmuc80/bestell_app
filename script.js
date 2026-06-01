893d28d9c2d4f63120081fea0e42b555ffaf130a
Maik <maikmuc80@gmail.com>
2026-06-01 08:27:31 +0200

    fix: array change

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const menu = [
   // Burger & Sandwiches
   {
     category: "Burger & Sandwiches",
+    image: "./assets/img/Veggie_mushroom_black_burger.jpg",
     name: "Veggie mushroom black burger",
     description: "Mixed green salad, Tomatoes, Edamame, Mushrooms",
     price: 16.90,
@@ -9,6 +10,7 @@ const menu = [
   },
   {
     category: "Burger & Sandwiches",
+    image: "./assets/img/All_meat_burger.jpg",
     name: "All meat burger",
     description: "Beef, Bacon, Dill pickles, Smoked cheese, Ketchup, BBQ sauce",
     price: 15.90,
@@ -16,6 +18,7 @@ const menu = [
   },
   {
     category: "Burger & Sandwiches",
+    image: "./assets/img/Beef_red_burger.jpg",
     name: "Beef red burger",
     description: "Beef, Cheese, Tomatoes, Lettuce, Onion",
     price: 14.90,
@@ -23,6 +26,7 @@ const menu = [
   },
   {
     category: "Burger & Sandwiches",
+    image: "./assets/img/BIg_chicken_burger.jpg",
     name: "Big chicken burger",
     description: "Chicken, Cheese, Tomatoes, Lettuce, Onion, Bell pepper",
     price: 15.90,
@@ -32,6 +36,7 @@ const menu = [
   // Pizza (30cm)
   {
     category: "Pizza",
+    image: "./assets/img/Pizza_Margherita.jpg",
     name: "Pizza Margherita",
     description: "Tomato Sauce, Mozzarella",
     price: 11.90,
@@ -39,6 +44,7 @@ const menu = [
   },
   {
     category: "Pizza",
+    image: "./assets/img/Pizza_Chorizo.jpg",
     name: "Pizza Chorizo",
     description: "Tomato slices, Mozzarella, Chorizo",
     price: 13.90,
@@ -46,6 +52,7 @@ const menu = [
   },
   {
     category: "Pizza",
+    image: "./assets/img/Funghi.jpg",
     name: "Funghi",
     description: "Red onion, Olives, Button Mushrooms, Mozzarella",
     price: 12.90,
@@ -53,6 +60,7 @@ const menu = [
   },
   {
     category: "Pizza",
+    image: "./assets/img/Quattro_Formaggi_with_Chicken.jpg",
     name: "Quattro Formaggi with Chicken",
     description: "Chicken, Mozzarella, Gorgonzola, Fontina, Parmigiano Reggiano",
     price: 15.90,
@@ -62,6 +70,7 @@ const menu = [
   // Salad
   {
     category: "Salad",
+    image: "./assets/img/Warm_beef_arugula_salad.jpg",
     name: "Warm beef arugula salad",
     description: "Beef, Arugula, Field salad, Greek feta, Cherry tomatoes, Sun-dried Tomatoes, Balsamic-vinegar dressing",
     price: 16.90,
@@ -69,6 +78,7 @@ const menu = [
   },
   {
     category: "Salad",
+    image: "./assets/img/Mini_green_Salad.jpg",
     name: "Mini green Salad",
     description: "Green salad, Cucumber, Carrots, Parsley, Radishes",
     price: 7.90,
@@ -76,6 +86,7 @@ const menu = [
   },
   {
     category: "Salad",
+    image: "./assets/img/Green_Salad_with_sea_food.jpg",
     name: "Green Salad with sea food",
     description: "Mixed greens, Cherry tomatoes, Red onion, Mussels, Squid rings, Shrimp, Dijon mustard-lemon dressing with dill",
     price: 16.90,
@@ -83,6 +94,7 @@ const menu = [
   },
   {
     category: "Salad",
+    image: "./assets/img/Vegan_green_salad_with_tofu.jpg",
     name: "Vegan green salad with tofu",
     description: "Green salad, Cherry tomatoes, Cucumber, Baby spinach, Edamame, Radishes, Bittercress, Tofu, Peanuts",
     price: 14.90,
@@ -95,3 +107,19 @@ function init(){
 
 }
 
+function renderCategory() {
+
+
+}
+
+function renderArticle() {
+    
+}
+
+function templateCategory() {
+
+}
+
+function templateArticle() {
+
+}
